63591a398b6fb4e9d1cb8f3e26e72cdd55321290
Nikolay Kamenev <kamenik1@icloud.com>
2022-06-11 13:38:11 +0300

    feat: added widget

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022061103;
+$plugin->version   = 2022061110;
 $plugin->requires  = 2022041200;
 $plugin->component = 'block_moyclass';
